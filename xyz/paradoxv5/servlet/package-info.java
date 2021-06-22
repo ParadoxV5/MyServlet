0f43d8508eeffa789cc7c12aab29b5d2865e6391
ParadoxV5 <paradox.ver5@gmail.com>
2021-06-22 17:27:31 -0600

    Moved some JavaDoc to `package-info` so it’s not `MyServlet` ruling all

--- xyz/paradoxv5/servlet/package-info.java
+++ xyz/paradoxv5/servlet/package-info.java
@@ -1,2 +1,7 @@
-/** See {@link xyz.paradoxv5.servlet.MyServlet} */
+/**
+  A collection of {@code abstract} classes in which I collect up some
+  repeated code I commonly run into while taking my Web Dev tutorial class.
+  
+  @author ParadoxV5
+*/
 package xyz.paradoxv5.servlet;
