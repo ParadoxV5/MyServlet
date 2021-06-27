bff1d1c87b9a2c43a8273181cb65349bb27950f9
ParadoxV5 <paradox.ver5@gmail.com>
2021-06-27 17:20:20 -0600

    Maximum abstraction reached… Rebranded `AbstractDatabase` to `DBConnectionPool` (no longer need a type parameter)

--- xyz/paradoxv5/servlet/DBConnectionPool.java
+++ xyz/paradoxv5/servlet/DBConnectionPool.java
@@ -1,26 +1,26 @@
 package xyz.paradoxv5.servlet;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 
 import java.sql.SQLException;
 import javax.naming.NamingException;
 
 /**
   TODO: Docs
- * @param <E> currently unused
 */
-public abstract class AbstractDatabase<E> {
+public class DBConnectionPool {
   protected final DataSource dataSource;
   
   /** TODO: Docs
    * @param resourceName
    * this must match your resource name in context.xml
    * @throws NamingException todo */
-  public AbstractDatabase(String resourceName) throws NamingException {
+  public DBConnectionPool(String resourceName) throws NamingException {
     dataSource = (DataSource)((new javax.naming.InitialContext()).lookup("java:/comp/env/" + resourceName));
   }
   
-  public synchronized java.sql.Connection getConnection() throws SQLException {
+  public synchronized Connection get() throws SQLException {
     return dataSource.getConnection();
   }
 }
