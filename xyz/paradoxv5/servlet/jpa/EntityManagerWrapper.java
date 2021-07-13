769e97ecd69ce936708613a8c0976c9bbcb6634f
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-13 02:18:08 -0600

    `EntityManagerWrapper` now always come with a transaction

--- xyz/paradoxv5/servlet/jpa/EntityManagerWrapper.java
+++ xyz/paradoxv5/servlet/jpa/EntityManagerWrapper.java
@@ -1,49 +1,32 @@
 package xyz.paradoxv5.servlet.jpa;
-
-import java.util.function.Supplier;
-
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 
 /**
   
 */
-public class EntityManagerWrapper implements Supplier<EntityManager>, AutoCloseable {
+public class EntityManagerWrapper implements java.util.function.Supplier<EntityManager>, AutoCloseable {
   protected final EntityManager entityManager;
-  
-  private EntityTransaction transaction;
-  public boolean isTransactioned() { return transaction != null; }
   
   public EntityManagerWrapper(EntityManager entityManager) {
     this.entityManager = entityManager;
+    entityManager.getTransaction().begin();
   }
   
-  @Override public EntityManager get() {
-    return entityManager;
-  }
+  @Override public EntityManager get() { return entityManager; }
   
-  public void beginTransaction() {
-    if(transaction != null) return;
-    transaction = get().getTransaction();
-    transaction.begin();
-  }
-  public void commitTransaction() {
-    if(transaction == null) return;
-    try { transaction.commit(); }
+  public void commit() {
+    try { entityManager.getTransaction().commit(); }
     catch(Exception e) {
-      rollbackTransaction();
+      rollback();
       throw e;
     }
-    transaction = null;
   }
-  public void rollbackTransaction() {
-    if(transaction == null) return;
-    transaction.rollback();
-    transaction = null;
+  public void rollback() {
+    entityManager.getTransaction().rollback();
   }
   
   @Override public void close() {
-    commitTransaction();
+    commit();
     entityManager.close();
   }
 }
