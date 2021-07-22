41c3c960ce5e6daf77448ac48f2f6f961831b084
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-21 18:10:13 -0600

    Added Javadoc for `EntityManagerWrapper`

--- xyz/paradoxv5/servlet/jpa/EntityManagerWrapper.java
+++ xyz/paradoxv5/servlet/jpa/EntityManagerWrapper.java
@@ -1,19 +1,52 @@
 package xyz.paradoxv5.servlet.jpa;
+import java.util.function.Supplier;
 import javax.persistence.EntityManager;
+// Javadoc imports not used by Java code
+import javax.persistence.EntityTransaction;
 
 /**
+  A wrapper class over {@link EntityManager} that automatically
+    {@linkplain EntityTransaction#begin() begins} a
+    {@linkplain EntityManager#getTransaction() transaction} in
+    {@linkplain #EntityManagerWrapper(EntityManager) constructor} and
+    {@linkplain EntityTransaction#commit() commits} it upon {@link #close()}.
+  <p>
+  This is a {@link Supplier} of {@code EntityManager} and uses the contracted
+    {@link #get()} as the getter for {@link #entityManager}.
   
+  @version 1.001
 */
-public class EntityManagerWrapper implements java.util.function.Supplier<EntityManager>, AutoCloseable {
+public class EntityManagerWrapper implements Supplier<EntityManager>, AutoCloseable {
+  /**
+    The wrapped {@code EntityManager};
+    the {@code public} getter is {@link #get()}
+    
+    @see EntityManagerWrapper
+    @see #close()
+  */
   protected final EntityManager entityManager;
   
+  /**
+    Takes in the {@code entityManager} to wrap and starts its
+    {@linkplain EntityManager#getTransaction() transaction}
+    
+    @param entityManager {@link #entityManager}
+    @see EntityManagerWrapper
+  */
   public EntityManagerWrapper(EntityManager entityManager) {
     this.entityManager = entityManager;
     entityManager.getTransaction().begin();
   }
   
+  /** @return {@link #entityManager} */
   @Override public EntityManager get() { return entityManager; }
   
+  /**
+    {@linkplain EntityTransaction#commit() Commit} the current
+    {@linkplain EntityManager#getTransaction() transaction}
+    
+    @see #entityManager
+  */
   public void commit() {
     try { entityManager.getTransaction().commit(); }
     catch(Exception e) {
@@ -21,10 +54,20 @@ public class EntityManagerWrapper implements java.util.function.Supplier<EntityM
       throw e;
     }
   }
+  /**
+    {@linkplain EntityTransaction#rollback() Rollback} the current
+    {@linkplain EntityManager#getTransaction() transaction}
+    
+    @see #entityManager
+  */
   public void rollback() {
     entityManager.getTransaction().rollback();
   }
   
+  /**
+    {@link #commit()} and then
+    {@linkplain EntityManager#close() close} the {@link #entityManager}
+  */
   @Override public void close() {
     commit();
     entityManager.close();
