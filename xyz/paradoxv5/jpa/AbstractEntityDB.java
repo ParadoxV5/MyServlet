7a87b00b230c7babe722c9d02642d02160d9bfa0
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-23 18:01:23 -0600

    Added Javadoc for `AbstractEntityDB`

--- xyz/paradoxv5/jpa/AbstractEntityDB.java
+++ xyz/paradoxv5/jpa/AbstractEntityDB.java
@@ -1,19 +1,31 @@
 package xyz.paradoxv5.jpa;
 
 import xyz.paradoxv5.CRUD;
-
-import java.util.Set;
-import java.util.function.Supplier;
-
-import javax.persistence.EntityManager;
 import java.io.Serializable;
+import java.util.function.Supplier;
+import javax.persistence.EntityManager;
+import java.util.stream.*;
 
-/**
-  
+/** The {@code abstract} superclass of a database class (a {@link CRUD}) for an {@link AbstractEntity} type
+  @param <E>
+  The type of entity elements the class CRUDs
+  @param <K>
+    The type of the entity’ primary key (“ID”), for {@link #get(Serializable)}
+  @apiNote
+    This superclass uses {@link EntityManagerWrapper}s (supplied by {@link #entityManagerSupplier} which is
+    initialized from {@link #AbstractEntityDB(Class, Supplier) the constructor}) to manage transactions
+  @version
+    1.01
 */
 public abstract class AbstractEntityDB<E extends AbstractEntity<K>, K extends Serializable> implements CRUD<E, K> {
+  /** The persistent constant holding {@code <E>}’s class, as compiling discards type variables */
   protected final Class<E> e;
+  /** The supplier that provides the {@code EntityManagerWrapper}s */
   protected final Supplier<EntityManagerWrapper> entityManagerSupplier;
+  /** {@link AbstractEntityDB} constructor
+    @param klass {@link #e}
+    @param entityManagerSupplier {@link #entityManagerSupplier}
+  */
   public AbstractEntityDB(Class<E> klass, Supplier<EntityManagerWrapper> entityManagerSupplier) {
     this.e = klass;
     this.entityManagerSupplier = entityManagerSupplier;
@@ -25,14 +37,31 @@ public abstract class AbstractEntityDB<E extends AbstractEntity<K>, K extends Se
     }
   }
   
+  /** The QL string for {@link #getAll()} */
   public abstract String getQlforGetAll();
-  @Override public Set<E> getAll() {
+  /** @apiNote
+    This implementation retrieves using the string provided by {@link #getQlforGetAll()} and
+    {@linkplain javax.persistence.TypedQuery#getResultStream() stream}
+    {@linkplain Stream#collect(Collector) to a}
+    {@linkplain Collectors#toSet() set}.
+  */
+  @Override public java.util.Set<E> getAll() {
     try(EntityManagerWrapper entityManager = entityManagerSupplier.get()) {
-      return entityManager.get().createQuery(getQlforGetAll(), e).getResultStream().collect(java.util.stream.Collectors.toSet());
+      return entityManager.get().createQuery(getQlforGetAll(), e).getResultStream().collect(Collectors.toSet());
     }
   }
   
+  /** Add the {@code entity} to the {@code entityManager}
+    @param entityManager the entity manager
+    @param entity the entity
+    @throws Exception if an exception {@code throw}s
+    @see #add(AbstractEntity)
+  */
   protected abstract void add0(EntityManager entityManager, E entity) throws Exception;
+  /** @apiNote
+    This preprogramming manages the {@code EntityManagerWrapper}; implementations provide
+    {@link #add0(EntityManager, AbstractEntity)} which does the processing part.
+  */
   @Override public synchronized void add(E entity) throws Exception {
     try(EntityManagerWrapper entityManager = entityManagerSupplier.get()) { try {
       add0(entityManager.get(), entity);
@@ -42,7 +71,17 @@ public abstract class AbstractEntityDB<E extends AbstractEntity<K>, K extends Se
     } }
   }
   
+  /** Update the {@code entity} in the {@code entityManager}
+    @param entityManager the entity manager
+    @param entity the entity
+    @throws Exception if an exception {@code throw}s
+    @see #update(AbstractEntity)
+  */
   protected abstract void update0(EntityManager entityManager, E entity) throws Exception;
+  /** @apiNote
+    This preprogramming manages the {@code EntityManagerWrapper}; implementations provide
+    {@link #update0(EntityManager, AbstractEntity)} which does the processing part.
+  */
   @Override public synchronized void update(E entity) throws Exception {
     try(EntityManagerWrapper entityManager = entityManagerSupplier.get()) { try {
       update0(entityManager.get(), entity);
@@ -52,7 +91,17 @@ public abstract class AbstractEntityDB<E extends AbstractEntity<K>, K extends Se
     } }
   }
   
+  /** Remove the {@code entity} from the {@code entityManager}
+    @param entityManager the entity manager
+    @param entity the entity
+    @throws Exception if an exception {@code throw}s
+    @see #remove(AbstractEntity)
+  */
   protected abstract void remove0(EntityManager entityManager, E entity) throws Exception;
+  /** @apiNote
+    This preprogramming manages the {@code EntityManagerWrapper}; implementations provide
+    {@link #remove0(EntityManager, AbstractEntity)} which does the processing part.
+  */
   @Override public synchronized void remove(E entity) throws Exception {
     try(EntityManagerWrapper entityManager = entityManagerSupplier.get()) { try {
       remove0(entityManager.get(), entity);
