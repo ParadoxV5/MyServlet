95d973dcc7ffe16106aaf360ee042bd18d3330e7
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-23 18:57:48 -0600

    `AbstractEntity`: `CRUD` import is Javadoc only

--- xyz/paradoxv5/jpa/AbstractEntity.java
+++ xyz/paradoxv5/jpa/AbstractEntity.java
@@ -1,9 +1,9 @@
 package xyz.paradoxv5.jpa;
 
-import xyz.paradoxv5.CRUD;
-
 import java.util.Objects;
 import java.io.Serializable;
+// Javadoc imports not used by Java code
+import xyz.paradoxv5.CRUD;
 
 /**
   My abstract superclass for an entity of {@linkplain AbstractEntityDB a database}.
