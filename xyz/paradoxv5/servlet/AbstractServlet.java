acb922b66d496de580072a9cb726229bdc41a937
ParadoxV5 <paradox.ver5@gmail.com>
2021-07-26 20:32:30 -0600

    Update AbstractServlet.java
    * Ordered `#forward` after `abstract` ‘get’s;
    * Deleted `#nonNullElseGet`
    * Incremented `@version`

--- xyz/paradoxv5/servlet/AbstractServlet.java
+++ xyz/paradoxv5/servlet/AbstractServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.logging.Logger;
-import java.util.function.Supplier;
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 
@@ -23,32 +21,13 @@ import javax.servlet.ServletResponse;
   This class also marks {@link #doGet(HttpServletRequest, HttpServletResponse)} and
   {@link #doPost(HttpServletRequest, HttpServletResponse)} {@code abstract} for its children.
   
-  @version 2.222…
+  @version 3
 */
 public abstract class AbstractServlet extends HttpServlet implements java.util.EventListener {
   private static final long serialVersionUID = 2;
   
   /** {@link Logger#getLogger(String) Logger of the name} {@code xyz.paradoxv5.servlet} (the package name, hardcoded) */
   public static final Logger LOGGER = Logger.getLogger("xyz.paradoxv5.servlet");
-  
-  /**
-    This method is equivalent to a backport of <a href="
-      https://docs.oracle.com/javase/9/docs/api/java/util/Objects.html#requireNonNullElseGet-T-java.util.function.Supplier-
-    ">{@code requireNonNullElseGet(T, }{@link Supplier}{@code )}
-    that’s added in Java 9</a>.
-    <br>
-    (I still had to use Java 8 for my {@link HttpServlet Servlet}s. Maybe because NetBeans still use Java 8?)
-    
-    @param obj
-      the object to test
-    @param supplier
-      the {@link Supplier} that this uses if {@code obj} is {@code null}
-    @return
-      {@code obj} if it’s not {@code null}, {@link Supplier#get() supplier.get()} if it is
-  */
-  public static <T> T nonNullElseGet​(T obj, Supplier<? extends T> supplier) {
-    return (obj == null) ? supplier.get() : obj;
-  }
   
   /**
     This invalidate the current {@link HttpSession session} if there is one.
@@ -61,6 +40,9 @@ public abstract class AbstractServlet extends HttpServlet implements java.util.E
     HttpSession oldSession = request.getSession(false);
     if(oldSession != null) oldSession.invalidate();
   }
+  
+  @Override protected abstract void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+  @Override protected abstract void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
   
   /**
     Forward the {@code request} and {@code response} to the resource at the given {@code path}
@@ -75,7 +57,4 @@ public abstract class AbstractServlet extends HttpServlet implements java.util.E
   protected void forward(String path, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     getServletContext().getRequestDispatcher(path).forward(request, response);
   }
-  
-  @Override protected abstract void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
-  @Override protected abstract void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
